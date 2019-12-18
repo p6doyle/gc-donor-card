da71bd2cd5597c4d860f0eeabfe0a3f1d747cd6a
Peyton Doyle <p6doyle@gmail.com>
2019-12-18 14:50:43 -0500

    closer than ever iterating over table!

--- donor-card/src/index.js
+++ donor-card/src/index.js
@@ -9,11 +9,20 @@ function fetchDonors(){
   fetch("http://localhost:3000/donors")
   .then(response => response.json())
   .then(donorArray => {
-    console.log(donorArray)
-    donorArray.forEach(donorObj => {
-      renderDonors(donorObj)
-    })})
-  }
+    let tbl = document.getElementById("donors");
+    let tblBody = document.createElement("tbody");
+    donorArray.forEach(donor => {
+      let row = document.createElement("tr");
+      row.appendChild(document.createTextNode(donor.name));
+      row.appendChild(document.createTextNode(donor.amount));
+      row.appendChild(document.createTextNode(donor.type));
+      tblBody.appendChild(row);
+    });
+
+    tbl.appendChild(tblBody);
+
+      }
+    )}
 
 // rendering donation bar on load
 function renderBar(){
@@ -30,13 +39,26 @@ function renderBar(){
 }
 
 function renderDonors(donorObj){
-  let card = document.querySelector("#card-back");
-  let donorList = document.createElement("ol");
-  let donorName = document.createElement("li")
+  let card = document.querySelector("#card-back")
+  // let newRow = document.createElement("tr")
+  let donorName = document.createElement("td")
   donorName.innerText = `${donorObj.name}`
+
+  // let donornames = document.querySelector("#donorName")
+  // let donordollars = document.querySelector("#donorDollars")
+  // let donortype = document.querySelector("#donarType")
+  //
+  // let donorName = document.createElement("li")
+  // let donorDollars = document.createElement("li")
+  // let donorType = document.createElement("li")
+  //
+  // donorName.innerText = `${donorObj.name}`
+  // donorDollars.innerText = `${donorObj.amount}`
+  // donorType.innerText = `${donorObj.type}`
   card.append(donorName)
+  //
   let cardContainer = document.querySelector("#container-back")
-  cardContainer.appendChild(donorList)
+  cardContainer.appendChild(donorName)
 }
 
 function flipCard() {
