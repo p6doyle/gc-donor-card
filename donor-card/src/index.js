6f626b3b32629cec87689ab77d59cf255113b6b3
Peyton Doyle <p6doyle@gmail.com>
2019-12-18 15:16:33 -0500

    added final total and give gift button to back

--- donor-card/src/index.js
+++ donor-card/src/index.js
@@ -9,6 +9,7 @@ function fetchDonors(){
   fetch("http://localhost:3000/donors")
   .then(response => response.json())
   .then(donorArray => {
+    renderFigures(donorArray);
     let tbl = document.getElementById("donors");
     let tblBody = document.createElement("tbody");
     donorArray.forEach(donor => {
@@ -42,29 +43,6 @@ function renderBar(){
   cardContainer.appendChild(donorMeter)
 }
 
-function renderDonors(donorObj){
-  let card = document.querySelector("#card-back")
-  // let newRow = document.createElement("tr")
-  let donorName = document.createElement("td")
-  donorName.innerText = `${donorObj.name}`
-
-  // let donornames = document.querySelector("#donorName")
-  // let donordollars = document.querySelector("#donorDollars")
-  // let donortype = document.querySelector("#donarType")
-  //
-  // let donorName = document.createElement("li")
-  // let donorDollars = document.createElement("li")
-  // let donorType = document.createElement("li")
-  //
-  // donorName.innerText = `${donorObj.name}`
-  // donorDollars.innerText = `${donorObj.amount}`
-  // donorType.innerText = `${donorObj.type}`
-  card.append(donorName)
-  //
-  let cardContainer = document.querySelector("#container-back")
-  cardContainer.appendChild(donorName)
-}
-
 function flipCard() {
   var x = document.getElementById("card-front");
   var y = document.getElementById("card-back");
@@ -77,6 +55,17 @@ function flipCard() {
     y.style.display = "block";
     x.style.display = "none";
   }
+}
+
+function renderFigures(donorArray) {
+  let total = donorArray.reduce((total, obj) => obj.amount + total,0)
+  console.log(total)
+  let backtotals = document.querySelector("#backtotal");
+  let backtotal = document.createElement("h2");
+  backtotal.innerHTML = "$" + total
+  backtotals.appendChild(backtotal)
+
+
 }
 
 
